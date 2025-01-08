a17910e7c47fb7a353fe08ed6b7ed1e34cb7e26d
Rudy <rudy.hariyanto@student.president.ac.id>
2025-01-08 14:23:59 +0700

    Update Absen.py

--- Page/Absen.py
+++ Page/Absen.py
@@ -45,7 +45,7 @@ with st.form(key='absensi_form'):
         submit_button = st.form_submit_button(label='Kirim')
 
        # Input keterangan lembur
-       st.text_input("Keterangan Lembur")
+       option=st.text_input("Keterangan Lembur")
        if keterangan_lembur:
            st.write(f"Keterangan Lembur: {keterangan_lembur}")
         
